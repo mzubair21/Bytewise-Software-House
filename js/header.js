88586fcb36e2af728d239d36354986d923f619f7
mzubair21 <mzubair21@outlook.com>
2020-11-03 07:28:29 +0500

    some major changes

--- js/header.js
+++ js/header.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+ /* Menu Toggler Click*/
+	$('.menu-toggler').click(function(){
+	 if($('.inner-width').hasClass('hide')){ 
+		 $('.inner-width').removeClass('hide');
+	 }
+	 else{
+		 $('.inner-width').addClass('hide');
+	 }
+  });
+	/* ----Menu Toggler Click-----*/
 	
+ /* Active Class Add */
+  $('.nav-buttons ul li').click(function(){
+   $('.nav-buttons ul li').removeClass('active');
+	  $(this).addClass('active')  
+  });
+/* Active Class Add */
 });
---
+++ js/loader.js
@@ -0,0 +1,4 @@
+$(window).on('load',function(){
+	$(".preloader").delay(1000).fadeOut('slow');
+	$('body').delay(1000).css("overflow",'visible');
+});
